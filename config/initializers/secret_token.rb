dc1c012c57d157068bf66fcea509baca5ef559d3
Christopher Nguyen <christoph.nguyen@gmail.com>
2013-05-31 20:06:33 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WellFormed::Application.config.secret_token = '4aebb8c0de5c3f2ed11a9e72125894ef5a4d40cc7c17fb560bff0d81ebe315d2d77be66cd4d0da24df18e08e8826780cb3e2f9fbf4e97c23bdfc0f2963adfb38'
